e9ecb124a62f4602c4ff9a91754101697d1c3a32
MD Shabbir Hossain <shabbirhossain660@gmail.com>
2023-12-25 18:36:58 +0600

    post and get all tasks using mongodb

--- index.js
+++ index.js
@@ -23,16 +23,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
+   
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const taskCollection = client.db('Task').collection('newTask');
+    app.post("/task", async(req,res)=>{
+       const task = req.body;
+       const result = await taskCollection.insertOne(task);
+       res.send(result);
+    })
+
+    app.get('/task', async(req,res)=>{
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    })
+    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
